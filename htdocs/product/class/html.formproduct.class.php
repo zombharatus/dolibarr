a038e0f67be2f7f4b26307c22e19ce9b0a0f86b9
Zombharatus <x@x.x>
2020-03-21 21:40:37 +0100

    Change default warehouse select array display

--- htdocs/product/class/html.formproduct.class.php
+++ htdocs/product/class/html.formproduct.class.php
@@ -176,6 +176,8 @@ class FormProduct
 	private function get_parent_path($tab, $final_label = '')
 	{
 		//phpcs:enable
+        return $tab['label'];
+
 		if(empty($final_label)) $final_label = $tab['label'];
 
 		if(empty($tab['parent_id'])) return $final_label;
