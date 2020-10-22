9fdd3d51944d064b7c7d0d7e65dfcdd10eccfeb9
Zombharatus <x@x.x>
2020-10-22 10:22:20 +0200

    Fix order to invoice not checked

--- htdocs/fourn/commande/orderstoinvoice.php
+++ htdocs/fourn/commande/orderstoinvoice.php
@@ -600,7 +600,7 @@ if (($action != 'create' && $action != 'add') && !$error) {
 
 			// Checkbox
 			print '<td class="center">';
-			print '<input class="flat checkformerge" type="checkbox" name="orders_to_invoice[]" value="'.$objp->rowid.'">';
+			print '<input class="flat checkformerge" type="checkbox" name="orders_to_invoice[]" value="'.$objp->rowid.'"  '.(in_array($objp->rowid, $selected)?'checked':'').' >';
 			print '</td>';
 
 			print '</tr>';
